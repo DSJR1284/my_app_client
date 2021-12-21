bf8dcc4e46045e7f61a609444cc9b6e87659075a
Doug Jones <DSJR1284@GMAIL.COM>
2021-12-21 14:29:34 -0500

    set up postsActions

--- src/components/PostsContainer.js
+++ src/components/PostsContainer.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import {fetchPosts} from '../actions/postsActions'
 
 class PostsContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchPosts()
+    }
+
 render() {
         return (
             <div>
@@ -10,4 +18,4 @@ render() {
     }
 }
 
-export default PostsContainer;
+export default connect(null, {fetchPosts})(PostsContainer);
