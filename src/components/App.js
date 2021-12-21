4702b70c945a700f2d1c3f89b307e174483b59b5
Doug Jones <DSJR1284@GMAIL.COM>
2021-12-21 12:10:25 -0500

    set up containers for post

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import BoardsContainer from './BoardsContainer'
+import PostsContainer from './PostsContainer'
 
 const App = () => {
   return (
     <div className="App">
       <BoardsContainer />
+      <PostsContainer />
     </div>
   );
 }
