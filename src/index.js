3887a394d025cb79e8d39be7b0ebe6e5b3d42295
Doug Jones <DSJR1284@GMAIL.COM>
2021-12-21 16:01:48 -0500

    setting up routing

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import { BrowserRouter as Router} from 'react-router-dom'
 
 import App from './components/App';
 import {rootReducer} from './reducers/rootReducer'
+
 
 const store = createStore(
   rootReducer,
@@ -16,7 +18,9 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store = {store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
